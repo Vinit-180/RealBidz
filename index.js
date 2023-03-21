fee978b30d636775f7d04a2d1e37b9d64914672d
Vinit-180 <vinitchokshi1809@gmail.com>
2023-03-22 00:33:23 +0530

    image_input_change_photos_details_add_js_add_logout and add_log_in with local storage

--- index.js
+++ index.js
@@ -1,33 +1,99 @@
 
 console.log("Function load now")
 var i = 0;
-var txt = 'Welcome to Realbidz';
+var txt = 'Welcome to Realbidz..';
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function welcome(){
+function welcome() {
   if (i < txt.length) {
     document.getElementById("Welcome").innerHTML += txt.charAt(i);
     i++;
-    setTimeout(welcome, speed);  
+    setTimeout(welcome, speed);
   }
-  else
-  {
-    var element=document.getElementById("Welcome");
-  var span = document.createElement("span");
-  // Set the innerHTML of the span element
-  span.innerHTML = ".";
+  else {
+    var element = document.getElementById("Welcome");
+    var span = document.createElement("span");
+    // Set the innerHTML of the span element
+    span.innerHTML = ".";
 
-  // Add the span element to the existing element
-  element.appendChild(span);
-  setInterval(function () {
-    if (element.length > txt) {
-      // var lastDot = dots[dots.length - 1];
-      span.style.visibility = span.style.visibility === "hidden" ? "visible" : "hidden";
+    // Add the span element to the existing element
+    element.appendChild(span);
+    setInterval(function () {
+      if (element.length > txt) {
+        // var lastDot = dots[dots.length - 1];
+        span.style.visibility = span.style.visibility === "hidden" ? "visible" : "hidden";
+      }
+      else {
+        span.style.visibility = span.style.visibility === "hidden" ? "visible" : "hidden";
+      }
+    }, 500);
+  }
+}
+function city() {
+    a = document.getElementById("inputCity").value;
+    b = document.getElementById("inputArea").value;
+    c = document.getElementById("inputType").value;
+    d = document.getElementById("inputCategory").value;
+    e = document.getElementById("inputPrice").value;
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(e)
+
+    const cls = document.getElementsByClassName("address1");
+    for (let i = 0; i < cls.length; i++) {
+      let text = ''
+      if (b != "Area/Village") {
+        text += b;
+      }
+      if (a != "Any") {
+        if (b != "Area/Village") {
+          text = text + " , " + a;
+        }
+        else {
+          text += a;
+        }
+      }
+      cls[i].innerHTML = text;
     }
-    else
+
+    const projectname = document.getElementById("project");
+
+    if (a != "Any") {
+      projectname.innerHTML = "Popular Projects In " + a;
+    }
+
+    else {
+      projectname.innerHTML = "Popular Projects In Gujarat ";
+    }
+    const property = document.getElementById("properties");
+
+    if (a != 'Any') {
+       property.innerHTML = "Properties In " + a;
+    }
+
+
+    image1 = "Images/Change_house_1.jpg";
+    image2 = "Images/Change_house_2.jpg"
+    image3 = "Images/Change_house_3.jpg"
+    image4 = "Images/house2_01.webp"
+    document.getElementById("img1").src = image1
+    document.getElementById("img2").src = image2
+    document.getElementById("img3").src = image3
+    document.getElementById("img4").src = image4
+
+    if(e!='Budget')
     {
-      span.style.visibility = span.style.visibility === "hidden" ? "visible" : "hidden";
+      document.getElementById("price1").innerHTML=e
+      document.getElementById("price2").innerHTML=e
+      document.getElementById("price3").innerHTML=e
+      document.getElementById("price4").innerHTML=e
     }
-  }, 500); 
 }
+function login1() {
+  document.getElementById("login-page").style.display = "block";
+  console.log("CSC")
 }
+
--- nav.js
+++ nav.js
@@ -2,9 +2,9 @@
 var input = document.getElementById("exampleInputPassword3");
 
 var text = document.getElementById("text");
-text.style.display="none";
+text.style.display = "none";
 // When the user presses any key on the keyboard, run the function
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function (event) {
 
   // If "caps lock" is pressed, display the warning text
   if (event.getModifierState("CapsLock")) {
@@ -16,12 +16,12 @@ input.addEventListener("keyup", function(event) {
 
 var input2 = document.getElementById("exampleInputPassword4");
 var text2 = document.getElementById("text2");
-text2.style.display="none";
+text2.style.display = "none";
 // When the user presses any key on the keyboard, run the function
-input2.addEventListener("keyup", function(event) {
+input2.addEventListener("keyup", function (event) {
   // If "caps lock" is pressed, display the warning text
   if (event.getModifierState("CapsLock")) {
-    input2.style.display="block";
+    input2.style.display = "block";
     text2.style.display = "block";
   } else {
     text2.style.display = "none"
@@ -41,18 +41,39 @@ input2.addEventListener("keyup", function(event) {
 //     text3.style.display = "none"
 //   }
 // });
+document.getElementById("alert-login").style.display = "none";
+document.getElementById('logout').style.display = "none";
+function login() {
+  mail = document.getElementById("exampleInputEmail1").value;
+  pswd = document.getElementById("exampleInputPassword3").value;
 
-var loggedUser=window.sessionStorage.getItem('1')
+  console.log(mail)
+  if (mail == '' && pswd == '') {
+    document.getElementById("alert-login").style.display = "block";
+  }
+  else {
+    document.getElementById("alert-login").style.display = "none";
+    localStorage.setItem("mail", mail)
+    // window.location.href = 'nav.html'
+  }
+}
+
+var loggedUser = window.localStorage.getItem("mail")
+console.log("111")
 console.log(loggedUser)
-const logout=document.querySelector('.logout')
-if(loggedUser!=null)
-{
-   document.getElementById('login').style.display="none";
-   document.getElementById('signUp1').style.display="none";
-//    logout.classList.add('active')
-document.getElementById('logout').style.display="block";
+const logout = document.querySelector('.logout')
+if (loggedUser != null) {
+  document.getElementById('login').style.display = "none";
+  document.getElementById('signUp1').style.display = "none";
+  //    logout.classList.add('active')
+  document.getElementById('logout').style.display = "block";
 }
-if(loggedUser==null)
-{
-  document.getElementById("logout").style.display="none";
+if (loggedUser == null) {
+  document.getElementById('logout').style.display = "none";
+  // window.location.href='index.html'
 }
+document.getElementById('logout').addEventListener('click', () => {
+  console.log('loggedOut')
+  window.localStorage.removeItem('mail')
+  window.location.href = 'index.html'
+})
