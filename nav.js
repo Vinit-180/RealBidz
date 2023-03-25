fc7b615f27dccdf92b15a1df9323180c668a3ad7
Vinit-180 <vinitchokshi1809@gmail.com>
2023-03-25 07:58:01 +0530

    final_validation_added

--- nav.js
+++ nav.js
@@ -33,9 +33,9 @@ input2.addEventListener("keyup", function (event) {
 
 var input3 = document.getElementById("exampleInputPassword5");
 var text3 = document.getElementById("text3");
-text3.style.display="none";
+text3.style.display = "none";
 // When the user presses any key on the keyboard, run the function
-input3.addEventListener("keyup", function(event) {
+input3.addEventListener("keyup", function (event) {
   console.log("function loading")
   // If "caps lock" is pressed, display the warning text
   if (event.getModifierState("CapsLock")) {
@@ -49,21 +49,18 @@ input3.addEventListener("keyup", function(event) {
 function login() {
   mail = document.getElementById("exampleInputEmail1").value;
   pswd = document.getElementById("exampleInputPassword3").value;
-
   console.log(mail)
   if (mail == '' && pswd == '') {
     document.getElementById("alert-login").style.display = "block";
     alert("Enter Email id");
   }
   else {
-    mail1=localStorage.getItem("mail");
-    pswd1=localStorage.getItem("pswd");
-    if(mail1!=mail && pswd!=pswd1)
-    {
-        alert("PASSWORD AND EMAIL ARE NOT MATCHED");
+    mail1 = localStorage.getItem("mail");
+    pswd1 = localStorage.getItem("pswd");
+    if (mail1 != mail && pswd != pswd1) {
+      alert("PASSWORD AND EMAIL ARE NOT MATCHED");
     }
-    else
-    {
+    else {
       document.getElementById("alert-login").style.display = "none";
     }
     // localStorage.setItem("mail", mail)
@@ -90,50 +87,83 @@ document.getElementById('logout').addEventListener('click', () => {
   window.localStorage.removeItem('email')
   window.location.href = 'index.html'
 })
-
-function validate()
+function valid()
 {
+  fname = document.forms["f1"]["fname"].value;
+  lname = document.forms["f1"]["lname"].value;
+  email = document.forms["f1"]["exampleInputEmail2"].value;
   f=1
-  fname=document.forms["f1"]["fname"].value;
-  lname=document.forms["f1"]["lname"].value;
-  email=document.forms["f1"]["exampleInputEmail2"].value;
-  number=document.forms["f1"]["phone"].value;
-  pswd1=document.forms["f1"]["exampleInputPassword4"].value;
-  pswd2=document.forms["f1"]["exampleInputPassword5"].value;
-  if(fname.length==0)
-  {
-    alert("Enter fname")
+  pat = /\W/;
+  // obj = document.getElementById("fname").value;
+  // obj2 = document.getElementById("lname").value;
+  if (pat.test(fname)) {
+    alert("Username should not have any special character");
     f=0
   }
-  else if(lname.length==0)
+  if(pat.test(lname))
   {
-    alert("Enter lname")
+    alert("Username should not have any special character");
     f=0
   }
-  else if(email.length==0)
+  pat2=/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+  if(!pat2.test(email))
   {
-    alert("Enter email id")
+    alert("Email id not perfect")
     f=0
   }
-  else if(number.length==0)
+  pat3=/^\d{10}$/
+  number = document.forms["f1"]["phone"].value;
+  if(!pat3.test(number))
   {
-    alert("Enter Mobile NUmber")
-    f=0
-  }
-  else if(pswd1.length==0 || pswd2.length==0 )
-  {
-    alert("Enter password");
-    f=0
-  }
-  if(pswd1 != pswd2)
-  {
-    alert("Both PASSWORDS ARE MUST BE SAME");
-    f=0
+      alert("Mobile Number not perfect")
+      f=0
   }
   if(f==1)
   {
-    localStorage.setItem("email",email)
-    localStorage.setItem("password",pswd2)
-    window.location.href='index.html'
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+function validate() {
+  f = 1
+  fname = document.forms["f1"]["fname"].value;
+  lname = document.forms["f1"]["lname"].value;
+  email = document.forms["f1"]["exampleInputEmail2"].value;
+  number = document.forms["f1"]["phone"].value;
+  pswd1 = document.forms["f1"]["exampleInputPassword4"].value;
+  pswd2 = document.forms["f1"]["exampleInputPassword5"].value;
+  if (fname.length == 0) {
+    alert("Enter fname")
+    f = 0
+  }
+  else if (lname.length == 0) {
+    alert("Enter lname")
+    f = 0
+  }
+  else if (email.length == 0) {
+    alert("Enter email id")
+    f = 0
+  }
+  else if (number.length == 0) {
+    alert("Enter Mobile NUmber")
+    f = 0
+  }
+  else if (pswd1.length == 0 || pswd2.length == 0) {
+    alert("Enter password");
+    f = 0
+  }
+  if (pswd1 != pswd2) {
+    alert("Both PASSWORDS ARE MUST BE SAME");
+    f = 0
+  }
+  f=valid()
+  if (f == 1) {
+    localStorage.setItem("email", email)
+    localStorage.setItem("password", pswd2)
+    window.location.href = 'index.html'
   }
 }
